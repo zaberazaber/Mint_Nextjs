579e2f44b62cec143899c7952160b4b2b30de8f9
zaberazaber <zubairansarijmi@gmail.com>
2024-07-20 20:03:28 +0530

    adding folder structure

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
@@ -17,7 +18,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src="./vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +32,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="./next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
