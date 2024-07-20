6e162a860cdd7d169ca11feea00ec019d9041856
zaberazaber <zubairansarijmi@gmail.com>
2024-07-20 21:12:03 +0530

    added prepush hook

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("hello world");
   return (
     <main className={styles.main}>
       <div className={styles.description}>
